22c08658d7c4739533995b6d8b9ca72ea9b316e3
senashine1026-lang <sena.shine1026@gmail.com>
2026-09-16 17:58:50 +0900

    aaa

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function ($) {
+  $(".firstview").bgSwitcher({
+    images: [
+      "images/background1.jpg",
+      "images/background2.jpg",
+      "images/background3.jpg",
+      "images/background4.jpg",
+      "images/background5.jpg",
+    ],
+    interval: 3000,
+    loop: true,
+  });
+  $(".slider").slick({
+    autoplay: false,
+    dots: true,
+    infinite: true,
+  });
+});
